3c8091c3e41b5b8652f5490ca7da79571d4da7e8
G. S. Varma <17321286+gsvarma@users.noreply.github.com>
2018-01-24 19:42:28 +0100

    Adds FOX examples.

---
+++ src/main/java/de/upb/cs/snlp/factchecking/FoxExample.java
@@ -0,0 +1,50 @@
+package de.upb.cs.snlp.factchecking;
+
+
+import org.aksw.fox.binding.java.FoxApi;
+import org.aksw.fox.binding.java.FoxParameter;
+import org.aksw.fox.binding.java.FoxResponse;
+import org.aksw.fox.binding.java.IFoxApi;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FoxExample {
+
+    public static void main(String[] a) throws Exception {
+        example_two();
+    }
+
+    public static void example_one() throws MalformedURLException {
+
+        IFoxApi fox = new FoxApi();
+        // URL api = new URL("http://0.0.0.0:4444/api");
+        // fox.setApiURL(api);
+
+        fox.setInput(new URL("https://en.wikipedia.org/wiki/Leipzig_University"));
+        fox.setOutputFormat(FoxParameter.OUTPUT.JSONLD);
+
+        FoxResponse response = fox.send();
+        System.out.println(response.getOutput());
+    }
+
+    public static void example_two() throws MalformedURLException {
+
+        IFoxApi fox = new FoxApi();
+        //URL api = new URL("http://0.0.0.0:4444/api");
+        URL api = new URL("http://139.18.2.164:4444/api");
+        fox.setApiURL(api);
+
+        fox.setTask(FoxParameter.TASK.NER);
+        fox.setOutputFormat(FoxParameter.OUTPUT.TURTLE);
+        fox.setLang(FoxParameter.LANG.EN);
+        fox.setInput("The philosopher and mathematician Leibniz was born in Leipzig.");
+        // fox.setLightVersion(FoxParameter.FOXLIGHT.ENStanford);
+
+        FoxResponse response = fox.send();
+
+        System.out.println(response.getInput());
+        System.out.println(response.getOutput());
+        System.out.println(response.getLog());
+    }
+}
