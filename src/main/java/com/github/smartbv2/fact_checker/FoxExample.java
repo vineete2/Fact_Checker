3df16595c5a4acaad7fb7befd07e2d7a8daa83ba
G. S. Varma <17321286+gsvarma@users.noreply.github.com>
2018-01-25 23:24:15 +0100

    Fixes FOX Example.

--- src/main/java/com/github/smartbv2/fact_checker/FoxExample.java
+++ src/main/java/com/github/smartbv2/fact_checker/FoxExample.java
@@ -3,7 +3,6 @@ package com.github.smartbv2.fact_checker;
 
 import org.aksw.fox.binding.java.FoxApi;
 import org.aksw.fox.binding.java.FoxParameter;
-import org.aksw.fox.binding.java.FoxResponse;
 import org.aksw.fox.binding.java.IFoxApi;
 
 import java.net.MalformedURLException;
@@ -11,40 +10,29 @@ import java.net.URL;
 
 public class FoxExample {
 
+    //public final static Logger LOG = LogManager.getLogger(FoxExample.class);
+
     public static void main(String[] a) throws Exception {
-        example_two();
+        example();
     }
 
-    public static void example_one() throws MalformedURLException {
+    public static void example() throws MalformedURLException {
 
-        IFoxApi fox = new FoxApi();
-        // URL api = new URL("http://0.0.0.0:4444/api");
-        // fox.setApiURL(api);
-
-        fox.setInput(new URL("https://en.wikipedia.org/wiki/Leipzig_University"));
-        fox.setOutputFormat(FoxParameter.OUTPUT.JSONLD);
-
-        FoxResponse response = fox.send();
-        System.out.println(response.getOutput());
-    }
-
-    public static void example_two() throws MalformedURLException {
-
-        IFoxApi fox = new FoxApi();
-        //URL api = new URL("http://0.0.0.0:4444/api");
-        URL api = new URL("http://139.18.2.164:4444/api");
-        fox.setApiURL(api);
+        final IFoxApi fox = new FoxApi();
+        fox.setApiURL(new URL("http://fox-demo.aksw.org/fox"));
 
         fox.setTask(FoxParameter.TASK.NER);
-        fox.setOutputFormat(FoxParameter.OUTPUT.TURTLE);
         fox.setLang(FoxParameter.LANG.EN);
-        fox.setInput("The philosopher and mathematician Leibniz was born in Leipzig.");
-        // fox.setLightVersion(FoxParameter.FOXLIGHT.ENStanford);
 
-        FoxResponse response = fox.send();
+        fox.setOutputFormat(FoxParameter.OUTPUT.TURTLE);
+        // fox.setOutputFormat(FoxParameter.OUTPUT.JSONLD);
 
-        System.out.println(response.getInput());
-        System.out.println(response.getOutput());
-        System.out.println(response.getLog());
+        fox.setInput("University of Paderborn is in Germany.");
+        // fox.setInput(new URL("https://en.wikipedia.org/wiki/India"));
+
+        // LOG.info(fox.send());
+        String response = fox.send();
+        System.out.println(response);
     }
+
 }
