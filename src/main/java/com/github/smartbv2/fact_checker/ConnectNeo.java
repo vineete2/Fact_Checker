ba4376b4604506909ab56b6463d5d0bb40278a43
G. S. Varma <17321286+gsvarma@users.noreply.github.com>
2018-01-29 20:54:35 +0100

    Add sample data and read data in Neo4J.

--- src/main/java/com/github/smartbv2/fact_checker/ConnectNeo.java
+++ src/main/java/com/github/smartbv2/fact_checker/ConnectNeo.java
@@ -2,9 +2,11 @@ package com.github.smartbv2.fact_checker;
 
 
 import org.neo4j.jdbc.Connection;
+import org.neo4j.jdbc.PreparedStatement;
+import org.neo4j.jdbc.ResultSet;
+import org.neo4j.jdbc.ResultSetMetaData;
 
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
@@ -20,17 +22,36 @@ public class ConnectNeo {
         try (Connection con = (Connection) DriverManager.getConnection(url, "neo4j", "varma")) {
 
             // Create a PreparedStatement and submit a Cypher query:
-            String query = "MATCH (tom {name: {1}}) RETURN tom"; // Movie database
+            // String query = "MATCH (tom {name: {1}}) RETURN tom"; // Movie database
+            String query = "CREATE (baeldung:Company {name:\"Baeldung\"}) "
+                    + "-[:owns]-> (tesla:Car {make: {1}, model: {2}})"
+                    + "RETURN baeldung, tesla";
 
-            try (PreparedStatement stmt = con.prepareStatement(query)) {
-                stmt.setString(1, "Tom Hanks");
+//            try (PreparedStatement stmt = con.prepareStatement(query)) {
+//                stmt.setString(1, "Tom Hanks");
+//
+//                // Iterate and print out the result set
+//                try (org.neo4j.jdbc.ResultSet rs = (org.neo4j.jdbc.ResultSet) stmt.executeQuery()) {
+//                    while (rs.next()) System.out.println(
+//                             "Person: " + rs.getString("name") + " born in " + rs.getInt("born"));
+//                           // rs.toString());
 
-                // Iterate and print out the result set
-                try (org.neo4j.jdbc.ResultSet rs = (org.neo4j.jdbc.ResultSet) stmt.executeQuery()) {
-                    while (rs.next()) System.out.println(
-                            // "Person: " + rs.getString("name") + " born in " + rs.getInt("born"));
-                            rs.toString());
+            try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(query)) {
+                stmt.setString(1, "tesla");
+                stmt.setString(2, "Model1X");
+
+                ResultSet rs = (ResultSet) stmt.executeQuery();
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnsNumber = rsmd.getColumnCount();
+                // Read data from result set
+                while (rs.next()) {
+                    for (int i = 1; i <= columnsNumber; i++) {
+                        if (i > 1) System.out.print(",  ");
+                        String columnValue = rs.getString(i);
+                        System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                    }
                 }
+
             }
         }
     }
